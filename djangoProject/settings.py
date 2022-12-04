e6020ba32007090dad9761d6de16a145133119f7
Yura192c <yra.mironchik2003@gmail.com>
2022-12-05 00:23:24 +0300

    add Product structure

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -106,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-GB'
 
 TIME_ZONE = 'Europe/Moscow'
 
@@ -119,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+#from django.conf.urls import url
+from . import views
+
 
 urlpatterns = [
-    path('',views.shop_home, name = 'shop_home'),
+    path('', views.product_list, name='product_list'),
+    path(r'^(?P<category_slug>[-\w]+)/$',
+        views.product_list,
+        name='product_list_by_category'),
+    path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        views.product_detail,
+        name='product_detail'),
+    #path('',views.product_list, name='product_list'),
+    #path('',views.shop_home, name='shop_home'),
 
 ]
