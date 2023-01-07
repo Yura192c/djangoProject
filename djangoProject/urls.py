964fe86e3a3a1fca74d7bfed93d546761e50bec3
Yura192c <fpm.mironchiYU@bsu.by>
2023-01-07 15:26:11 +0300

    add order structure

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
     #path('cart/', include('cart.urls')),
     path('shop/', include('shop.urls')),
+    path('orders/', include('orders.urls', namespace='orders')),
     #path('shop/', include(('shop.urls', 'shop'), namespace='shop'))
 ]  # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
